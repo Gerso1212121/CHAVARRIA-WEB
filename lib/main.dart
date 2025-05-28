b7c27ce2b19a62b69307b2896aac78bac9c320cd
Gerson <gamewil50@gmail.com.com>
2025-05-28 17:30:22 -0600

    actualizacion optimizacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:final_project/spash.dart';
 import 'package:final_project/viewmodels/productos/carrito_viewmodel.dart';
-import 'package:final_project/views/home/sections/conditions/terms_conditions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -8,18 +8,16 @@ import 'package:final_project/views/home/home_screen.dart';
 import 'package:final_project/data/services/products_service.dart';
 import 'package:final_project/repositories/productos_usuario.dart';
 import 'package:final_project/viewmodels/productos/productos_viewmodel.dart';
+import 'package:final_project/views/home/sections/conditions/terms_conditions.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
     url: 'https://xuzvixzgudjycuywwppu.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1enZpeHpndWRqeWN1eXd3cHB1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NjQ5NzYxMCwiZXhwIjoyMDYyMDczNjEwfQ.vfsDcpR9KvFaZhA1zdUnXCS-8ozc_YHJcNIaT_FI9V4',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1enZpeHpndWRqeWN1eXd3cHB1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NjQ5NzYxMCwiZXhwIjoyMDYyMDczNjEwfQ.vfsDcpR9KvFaZhA1zdUnXCS-8ozc_YHJcNIaT_FI9V4',
   );
 
-  // ✅ Instancia de servicio y repositorio
   final productService = ProductService();
   final repository = ProductRepository(productService);
 
@@ -27,11 +25,13 @@ void main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (_) => ProductViewModel(repository)..loadProducts(),
+          create: (_) {
+            final vm = ProductViewModel(repository);
+            Future.microtask(() => vm.loadProducts()); // ✅ Carga diferida
+            return vm;
+          },
         ),
-        ChangeNotifierProvider(
-          create: (_) => CartViewModel(),
-        ),
+        ChangeNotifierProvider(create: (_) => CartViewModel()),
       ],
       child: const MyApp(),
     ),
@@ -47,9 +47,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomePage(),
+        '/': (context) => const SplashScreen(),
+        '/home': (context) => const HomePage(),
         '/terminos': (context) => const TermsAndConditionsPage(),
-        // Agrega aquí otras rutas si las necesitas
       },
     );
   }
